aeef341bb637962bf68e06f73241eef5551aea2d
KevinZu <zukeqiang@foxmail.com>
2019-01-22 13:27:29 +0800

    Update get_my_faces.py
    添加替代opcv做人脸提取的备选函数

--- contributed/get_my_faces.py
+++ contributed/get_my_faces.py
@@ -82,7 +82,62 @@ def CatchUsbVideo(out_queue):
     cap.release()
     cv2.destroyAllWindows() 
 
+def FaceDectectionUseTensorflowMtcnn(in_queue):
+    margin = 32
+    index_num = 0
+    target_dir = os.path.join(data_dir,"images_data_%d" % target_img_size)
+    output_user_dir = os.path.join(target_dir, name)
+    if not os.path.exists(output_user_dir):
+        os.makedirs(output_user_dir)
 
+    minsize = 20 # minimum size of face
+    threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
+    factor = 0.709 # scale factor
+
+
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
+    while 1:
+        img = in_queue.get()
+        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+        nrof_faces = bounding_boxes.shape[0]
+        if nrof_faces>0:
+            det = bounding_boxes[:,0:4]
+            det_arr = []
+            img_size = np.asarray(img.shape)[0:2]
+            print("====nrof_face: %d  img size:"  % nrof_faces)
+            print(img_size) 
+            if nrof_faces == 1:
+                bounding_box_size = (det[:,2]-det[:,0])*(det[:,3]-det[:,1])
+                img_center = img_size / 2
+                offsets = np.vstack([ (det[:,0]+det[:,2])/2-img_center[1], (det[:,1]+det[:,3])/2-img_center[0] ])
+                offset_dist_squared = np.sum(np.power(offsets,2.0),0)
+                index = np.argmax(bounding_box_size-offset_dist_squared*2.0) # some extra weight on the centering
+                det_arr.append(det[index,:])
+            for i, det in enumerate(det_arr):
+                det = np.squeeze(det)
+                bb = np.zeros(4, dtype=np.int32)
+                bb[0] = np.maximum(det[0]-margin/2, 0)
+                bb[1] = np.maximum(det[1]-margin/2, 0)
+                bb[2] = np.minimum(det[2]+margin/2, img_size[1])
+                bb[3] = np.minimum(det[3]+margin/2, img_size[0])
+                cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+                scaled = misc.imresize(cropped, (target_img_size, target_img_size), interp='bilinear')
+
+                output_filename = os.path.join(output_user_dir, ("%d" % index_num) +'.png')
+
+                filename_base, file_extension = os.path.splitext(output_filename)
+
+                output_filename_n = "{}{}".format(filename_base, file_extension)
+
+                misc.imsave(output_filename_n, scaled)
+
+
+                index_num += 1
+                
 def FaceDectection(in_queue):
     index = 0
     while 1:
